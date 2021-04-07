89d80aabaf49238228b567ad65c3eb4a485e11df
EsayH <ez@EsayHs-Mac-mini.local>
2021-04-07 14:56:33 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import './App.css';
+import Header from "./Header.js"
+
+function App() {
+  return (
+    <div className="app">
+
+      <Header />
+      
+      {/* App body */}
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widget */}
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyCdEPe5osp2QqOk6kIi1g-RSdx3vgVzTd0',
+	authDomain: 'facebook-clone-5990c.firebaseapp.com',
+	projectId: 'facebook-clone-5990c',
+	storageBucket: 'facebook-clone-5990c.appspot.com',
+	messagingSenderId: '526385653784',
+	appId: '1:526385653784:web:62ec3a2cc55df73fb9bb6d',
+	measurementId: 'G-0MT1HJF3FT',
+}
